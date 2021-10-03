e2392f32135a4b166656aa20959e72f014609102
Saulight <ninimeme73@gmail.com>
2021-10-03 11:01:26 +0200

    tout est pret et fonctionne jusqua la fin du TP

--- ConsoleApp1/ConsoleApp1/Library.cs
+++ ConsoleApp1/ConsoleApp1/Library.cs
@@ -12,4 +12,5 @@ namespace Main{
 
         public int heures;
     }
+    
 }
--- ConsoleApp1/ConsoleApp1/Main.cs
+++ ConsoleApp1/ConsoleApp1/Main.cs
@@ -1,23 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace Main{
     class Program{
         static void Main(){
             bool choix = true;
             List<Matiere> lP = new List<Matiere>();
+            int Totalheures = 0;
+            String Totalheuresstring = "";
             while (choix == true){
                 // menu de saisie
                 Console.WriteLine("Que voulez vous faire ?");
                 Console.WriteLine("1. Creer une nouvelle Matière");
                 Console.WriteLine("2. Afficher les matières");
-                Console.WriteLine("3. Quitter");
+                Console.WriteLine("3. Afficher le nombre total d'heures");
+                Console.WriteLine("4. Quitter");
                 string choixUtilisateur = Console.ReadLine();
-                if (choixUtilisateur == "1"){
+                if (choixUtilisateur == "1")
+                {
                     Matiere nouvellematiere = CreerMatiere();
                     lP.Add(nouvellematiere);
                 }
-                else if (choixUtilisateur == "2"){
+                else if (choixUtilisateur == "2")
+                {
                     Console.WriteLine("***********************************");
                     foreach (Matiere matiereinboucle in lP)
                     {
@@ -26,11 +32,20 @@ namespace Main{
                         Console.WriteLine("***********************************");
                     }
                 }
-                else if(choixUtilisateur == "3"){
+                else if (choixUtilisateur == "3")
+                {
+                    NumberFormatInfo nfi = new CultureInfo("en-US", false).NumberFormat;
+                    nfi.CurrencyGroupSeparator = ".";
+                    Totalheures = Claculsommeheures(lP);
+                    Totalheuresstring = Totalheures.ToString("C", nfi);
+                    Console.WriteLine("le nombre total d'heures vaut : " + Totalheuresstring.Substring(1, Totalheuresstring.Length - 4));
+                }
+                else if (choixUtilisateur == "4")
+                {
                     choix = false;
                 }
-                
-            }
+
+                }
         }
         public static Matiere CreerMatiere(){
             Matiere m = new Matiere();
@@ -87,6 +102,15 @@ namespace Main{
             string message = "";
             message = "Matière : " + m.Nom + ", Code : " + m.Code + ", Nb heures de la matière : "+ m.heures;
             return message;
+        }
+        public static int Claculsommeheures(List<Matiere> Lm)
+        {
+            int total = 0;
+            foreach (Matiere matiereinboucle in Lm)
+            {
+                total += matiereinboucle.heures;
+            }
+            return total;
         }
     }
 }
